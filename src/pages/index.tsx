d84b57aa086beab0d0e45c4b1a87a1646d95b86e
nairol203 <f.bock.2003@gmail.com>
2022-12-02 01:37:03 -0800

    fix for now playing error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,10 @@ import { Card, Chip, Flex, Group, Placeholder } from '@components/styles/Core.st
 import { trpc } from '@lib/trpc';
 import Image from 'next/image';
 
-// function calcTime(date: Date) {
-// 	const ms = Date.now() - date.getTime();
-// 	const hours = Math.floor(ms / 3600000) % 24;
-// 	const seconds = Math.floor(ms / 1000) % 60;
-// 	const minutes = Math.floor(ms / 60000) % 60;
-
-// 	if (hours > 0) return `vor ${hours} Stunde${hours === 1 ? '' : 'n'}`;
-// 	if (minutes > 0) return `vor ${minutes} Minute${minutes === 1 ? '' : 'n'}`;
-// 	return `vor ${seconds} Sekunde${seconds === 1 ? '' : 'n'}`;
-// }
-
 export default function Home() {
-	const currentlyPlaying = trpc.currentlyPlaying.useQuery();
 	const topTracks = trpc.recentlyPlayed.useQuery();
 
-	if (!topTracks.data || !currentlyPlaying.data)
+	if (!topTracks.data)
 		return (
 			<Flex direction='column' gap='1em' style={{ padding: '1em' }}>
 				<h1>Loading...</h1>
@@ -27,40 +15,7 @@ export default function Home() {
 
 	return (
 		<Flex direction='column' gap='1em' style={{ padding: '1em' }}>
-			{currentlyPlaying.data.is_playing && currentlyPlaying.data.currently_playing_type == 'track' && currentlyPlaying.data?.item && (
-				<>
-					<h1>Currently Playing</h1>
-					<Card key={currentlyPlaying.data?.item.id}>
-						<Flex gap='1em' align='center'>
-							<a style={{ display: 'flex', justifyContent: 'center' }} href={currentlyPlaying.data.item.external_urls.spotify} target='_blank' rel='noreferrer'>
-								<Image
-									src={
-										// @ts-expect-error
-										currentlyPlaying.data?.item.album.images[0].url
-									}
-									height={75}
-									width={75}
-									alt='Album Cover'
-									style={{ borderRadius: '0.25em' }}
-								/>
-							</a>
-							<Flex gap='.25em' direction='column'>
-								<h3>{currentlyPlaying.data?.item.name}</h3>
-								<Group>
-									{
-										// @ts-expect-error
-										currentlyPlaying.data?.item.artists.map(artist => (
-											<Chip clickable href={artist.external_urls.spotify} target='_blank' rel='noreferrer' key={artist.id} variant='secondary'>
-												{artist.name}
-											</Chip>
-										))
-									}
-								</Group>
-							</Flex>
-						</Flex>
-					</Card>
-				</>
-			)}
+			<CurrentlyPlaying />
 			<h1>Recently Played</h1>
 			<div style={{ display: 'flex', flexDirection: 'column', gap: '1em' }}>
 				{topTracks.data.items.map(item => (
@@ -84,5 +39,64 @@ export default function Home() {
 				))}
 			</div>
 		</Flex>
+	);
+}
+
+function CurrentlyPlaying() {
+	const currentlyPlaying = trpc.currentlyPlaying.useQuery();
+
+	if (!currentlyPlaying.data) return <></>;
+
+	return (
+		<>
+			<h1>Currently Playing</h1>
+			<Card
+				key={
+					// @ts-expect-error
+					currentlyPlaying.data?.item.id
+				}
+			>
+				<Flex gap='1em' align='center'>
+					<a
+						style={{ display: 'flex', justifyContent: 'center' }}
+						href={
+							// @ts-expect-error
+							currentlyPlaying.data.item.external_urls.spotify
+						}
+						target='_blank'
+						rel='noreferrer'
+					>
+						<Image
+							src={
+								// @ts-expect-error
+								currentlyPlaying.data?.item.album.images[0].url
+							}
+							height={75}
+							width={75}
+							alt='Album Cover'
+							style={{ borderRadius: '0.25em' }}
+						/>
+					</a>
+					<Flex gap='.25em' direction='column'>
+						<h3>
+							{
+								// @ts-expect-error
+								currentlyPlaying.data?.item.name
+							}
+						</h3>
+						<Group>
+							{
+								// @ts-expect-error
+								currentlyPlaying.data?.item.artists.map(artist => (
+									<Chip clickable href={artist.external_urls.spotify} target='_blank' rel='noreferrer' key={artist.id} variant='secondary'>
+										{artist.name}
+									</Chip>
+								))
+							}
+						</Group>
+					</Flex>
+				</Flex>
+			</Card>
+		</>
 	);
 }
