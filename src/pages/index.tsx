c051b515c855c554ac013780d14b2fd974758eea
Florian Bock <f.bock.2003@gmail.com>
2023-03-04 23:25:05 +0100

    Hover Dark Mode Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,7 +68,7 @@ export default function Home() {
 
 export const SkeletonObject: React.FC<{ type: 'track' | 'album'; ranking?: boolean }> = ({ type, ranking }) => {
 	return (
-		<div className='flex items-center justify-between gap-2 rounded-[4px] p-3 hover:bg-black hover:bg-opacity-10 hover:dark:bg-white'>
+		<div className='flex items-center justify-between gap-2 rounded-[4px] p-3 hover:bg-black hover:bg-opacity-10 hover:dark:bg-white hover:dark:bg-opacity-10'>
 			<div className='flex items-center gap-4'>
 				{ranking && <div className='skeleton flex w-5 justify-center'>1</div>}
 				<div className='skeleton h-[50px] w-[50px]'></div>
@@ -96,7 +96,7 @@ function CurrentlyPlaying() {
 	return (
 		<div className='grid gap-1'>
 			<h2>Currently Playing</h2>
-			<div className='flex items-center justify-between gap-2 rounded-[4px] p-3 hover:bg-black hover:bg-opacity-10 hover:dark:bg-white'>
+			<div className='flex items-center justify-between gap-2 rounded-[4px] p-3 hover:bg-black hover:bg-opacity-10 hover:dark:bg-white hover:dark:bg-opacity-10'>
 				<div className='flex items-center gap-4'>
 					<a href={currentlyPlaying.data.item?.external_urls.spotify} target='_blank' rel='noreferrer'>
 						<Image
