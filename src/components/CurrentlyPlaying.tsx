f0567c1e22a512971cd49e0ca4ca56c7313693fb
Florian Bock <f.bock.2003@gmail.com>
2023-03-06 11:28:41 +0100

    Updated Not Playing

--- src/components/CurrentlyPlaying.tsx
+++ src/components/CurrentlyPlaying.tsx
@@ -1,4 +1,4 @@
-import { faBackwardStep, faBars, faForwardStep, faPause } from '@fortawesome/free-solid-svg-icons';
+import { faBackwardStep, faBars, faForwardStep, faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { msToString } from '@lib/helpers';
 import { trpc } from '@lib/trpc';
@@ -8,9 +8,9 @@ export default function CurrentlyPlaying() {
     // @ts-expect-error
     const currentlyPlaying = trpc.currentlyPlaying.useQuery({}, { refetchInterval: 1000 });
 
-    if (currentlyPlaying.data && currentlyPlaying.data.is_playing && currentlyPlaying.data.currently_playing_type === 'track') {
+    if (currentlyPlaying.data?.currently_playing_type === 'track') {
         return (
-            <div className='z-10 flex h-full w-full items-center justify-between border-t-[1px] border-t-[#bebebe] dark:border-t-[#282828] bg-card px-4 dark:bg-darkMode-card sm:grid sm:grid-cols-3'>
+            <div className='z-10 flex h-full w-full items-center justify-between border-t-[1px] border-t-[#bebebe] bg-card px-4 dark:border-t-[#282828] dark:bg-darkMode-card sm:grid sm:grid-cols-3'>
                 <div className='flex gap-4'>
                     <div className='relative h-12 w-12 sm:h-16 sm:w-16'>
                         <Image
@@ -47,15 +47,15 @@ export default function CurrentlyPlaying() {
                     </div>
                 </div>
                 <div className='hidden justify-center gap-3 sm:grid'>
-                    <div className='flex justify-center'>
+                    <div className='flex justify-center gap-2'>
                         <button disabled>
-                            <FontAwesomeIcon height={40} width={40} icon={faBackwardStep} />
+                            <FontAwesomeIcon height={25} width={25} icon={faBackwardStep} />
                         </button>
                         <button disabled>
-                            <FontAwesomeIcon height={40} width={40} icon={faPause} />
+                            <FontAwesomeIcon height={25} width={25} icon={currentlyPlaying.data.is_playing ? faPause : faPlay} />
                         </button>
                         <button disabled>
-                            <FontAwesomeIcon height={40} width={40} icon={faForwardStep} />
+                            <FontAwesomeIcon height={25} width={25} icon={faForwardStep} />
                         </button>
                     </div>
                     <div className='flex items-center gap-4'>
@@ -71,7 +71,7 @@ export default function CurrentlyPlaying() {
                 </div>
                 <div className='hidden justify-end sm:flex'>
                     <button disabled>
-                        <FontAwesomeIcon height={40} width={40} icon={faBars} />
+                        <FontAwesomeIcon height={30} width={30} icon={faBars} />
                     </button>
                 </div>
                 <div className='absolute bottom-0 h-1 w-11/12 rounded-full bg-white dark:bg-gray-600 sm:hidden'>
@@ -84,5 +84,43 @@ export default function CurrentlyPlaying() {
         );
     }
 
-    return <div className='h-full border-t-[1px] border-t-[#282828] bg-card dark:bg-darkMode-card'>Nothing playing...</div>;
+    return (
+        <div className='z-10 flex h-full w-full items-center justify-between border-t-[1px] border-t-[#bebebe] bg-card px-4 dark:border-t-[#282828] dark:bg-darkMode-card sm:grid sm:grid-cols-3'>
+            <div className='flex gap-4'>
+                <div className='relative h-12 w-12 sm:h-16 sm:w-16'></div>
+                <div className='flex flex-col justify-center'>
+                    <a className='hover:underline' href='#' target='_blank' rel='noreferrer'></a>
+                    <div className='flex gap-1'></div>
+                </div>
+            </div>
+            <div className='hidden justify-center gap-3 sm:grid'>
+                <div className='flex justify-center gap-2'>
+                    <button disabled>
+                        <FontAwesomeIcon height={25} width={25} icon={faBackwardStep} />
+                    </button>
+                    <button disabled>
+                        <FontAwesomeIcon height={25} width={25} icon={faPlay} />
+                    </button>
+                    <button disabled>
+                        <FontAwesomeIcon height={25} width={25} icon={faForwardStep} />
+                    </button>
+                </div>
+                <div className='flex items-center gap-4'>
+                    <span className='text-xs'>{msToString(0)}</span>
+                    <div className='h-1.5 w-52 rounded-full bg-white dark:bg-gray-600 md:w-96 lg:w-[30rem]'>
+                        <div className='h-1.5 w-0 rounded-full bg-black dark:bg-white' />
+                    </div>
+                    <span className='text-xs'>{msToString(0)}</span>
+                </div>
+            </div>
+            <div className='hidden justify-end sm:flex'>
+                <button disabled>
+                    <FontAwesomeIcon height={30} width={30} icon={faBars} />
+                </button>
+            </div>
+            <div className='absolute bottom-0 h-1 w-11/12 rounded-full bg-white dark:bg-gray-600 sm:hidden'>
+                <div className='h-1 w-0 rounded-full bg-black dark:bg-white' />
+            </div>
+        </div>
+    );
 }
