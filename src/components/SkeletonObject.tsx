419b2a82ac74191eb533ab8c4cfe1f8683d9d70d
Florian Bock <f.bock.2003@gmail.com>
2023-03-06 11:11:25 +0100

    Skeleton was off

--- src/components/SkeletonObject.tsx
+++ src/components/SkeletonObject.tsx
@@ -1,6 +1,6 @@
 export const SkeletonObject: React.FC<{ type: 'track' | 'album'; ranking?: boolean }> = ({ type, ranking }) => {
 	return (
-		<div className='flex items-center justify-between gap-2 rounded-[4px] p-3 hover:bg-black hover:bg-opacity-10 hover:dark:bg-white hover:dark:bg-opacity-10'>
+		<div className='flex items-center justify-between gap-2 rounded-[4px] px-4 p-2 hover:bg-black hover:bg-opacity-10 hover:dark:bg-white hover:dark:bg-opacity-10'>
 			<div className='flex items-center gap-4'>
 				{ranking && <div className='skeleton flex w-5 justify-center'>1</div>}
 				<div className='skeleton h-[50px] w-[50px]'></div>
