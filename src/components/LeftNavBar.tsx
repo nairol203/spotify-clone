f561898d48437609c76c83db6e862c3235df1542
Florian Bock <f.bock.2003@gmail.com>
2023-03-06 18:01:00 +0100

    bigger hitbox to click

--- src/components/LeftNavBar.tsx
+++ src/components/LeftNavBar.tsx
@@ -24,44 +24,47 @@ export default function LeftNavBar() {
 				/>
 				<h1>Nairol Spotify</h1>
 			</Link>
-			<div className='m-2 flex flex-col gap-4'>
-				<Link className={`${router.pathname === '/' ? 'text-white' : 'hover:text-white'} flex items-center gap-4 text-gray-300`} href='/'>
+			<div>
+				<Link className={`${router.pathname === '/' ? 'text-white' : 'hover:text-white'} flex items-center gap-4 p-2 text-gray-300`} href='/'>
 					<FontAwesomeIcon icon={faHome} height={20} width={20} />
 					<span>Home</span>
 				</Link>
-				<Link className={`${router.pathname === '/top/artists' ? 'text-white' : 'hover:text-white'} flex items-center gap-4 text-gray-300`} href='/top/artists'>
+				<Link className={`${router.pathname === '/top/artists' ? 'text-white' : 'hover:text-white'} flex items-center gap-4 p-2 text-gray-300`} href='/top/artists'>
 					<FontAwesomeIcon icon={faChartLine} height={20} width={20} />
 
 					<span>Top Artists</span>
 				</Link>
-				<Link className={`${router.pathname === '/top/tracks' ? 'text-white' : 'hover:text-white'} flex items-center gap-4 text-gray-300`} href='/top/tracks'>
+				<Link className={`${router.pathname === '/top/tracks' ? 'text-white' : 'hover:text-white'} flex items-center gap-4 p-2 text-gray-300`} href='/top/tracks'>
 					<FontAwesomeIcon icon={faChartLine} height={20} width={20} />
 					<span>Top Tracks</span>
 				</Link>
 			</div>
 			<div className='h-0.5 w-full rounded-full bg-gray-500' />
-			<div className='mx-2 flex flex-col gap-4'>
-				<button disabled onClick={() => null} className='flex cursor-not-allowed items-center gap-4 text-gray-300'>
+			<div>
+				<button disabled onClick={() => null} className='flex cursor-not-allowed items-center gap-4 p-2 text-gray-300'>
 					<FontAwesomeIcon icon={faPlus} height={20} width={20} />
 					<span>Playlist erstellen</span>
 				</button>
-				<Link href='/collection/tracks' className={`${router.pathname === '/collection/tracks' ? 'text-white' : 'hover:text-white'} flex items-center gap-4 text-gray-300`}>
+				<Link
+					href='/collection/tracks'
+					className={`${router.pathname === '/collection/tracks' ? 'text-white' : 'hover:text-white'} flex items-center gap-4 p-2 text-gray-300`}
+				>
 					<FontAwesomeIcon icon={faHeart} height={20} width={20} />
 					<span>Lieblingssongs</span>
 				</Link>
-				<button disabled onClick={() => null} className='flex cursor-not-allowed items-center gap-4 text-gray-300'>
+				<button disabled onClick={() => null} className='flex cursor-not-allowed items-center gap-4 p-2 text-gray-300'>
 					<FontAwesomeIcon icon={faBookmark} height={20} width={20} />
 					<span>Deine Folgen</span>
 				</button>
 			</div>
 			<div className='h-0.5 w-full rounded-full bg-gray-500' />
-			<div className='flex flex-col gap-1'>
+			<div className='flex flex-col'>
 				{playlists.data?.items &&
 					playlists.data.items.map((playlist, index) => (
 						<Link
 							className={`${
 								router.asPath === `/playlist/${playlist.id}` ? 'text-white' : 'hover:text-white'
-							} overflow-hidden text-ellipsis whitespace-nowrap text-gray-300`}
+							} overflow-hidden text-ellipsis whitespace-nowrap p-0.5 text-gray-300`}
 							key={playlist.id + index}
 							href={`/playlist/${playlist.id}`}
 						>
