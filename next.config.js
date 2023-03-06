1fb0de486864a1832e54b4f7f1c1ef544c1aa20e
Florian Bock <f.bock.2003@gmail.com>
2023-03-06 17:47:57 +0100

    added image domain

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    i18n: {
-        locales: ['de'],
-        defaultLocale: 'de',
-    },
-    images: {
-        domains: ['i.scdn.co', 'mosaic.scdn.co'],
-    },
+	reactStrictMode: true,
+	swcMinify: true,
+	i18n: {
+		locales: ['de'],
+		defaultLocale: 'de',
+	},
+	images: {
+		domains: ['i.scdn.co', 'mosaic.scdn.co', 'wrapped-images.spotifycdn.com'],
+	},
 };
 
 module.exports = nextConfig;
