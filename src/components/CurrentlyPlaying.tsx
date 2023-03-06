79787617ac1e70e4d86e16b8f97e276cd03df232
Florian Bock <f.bock.2003@gmail.com>
2023-03-06 09:40:02 +0100

    Mobile Progress Bar

--- src/components/CurrentlyPlaying.tsx
+++ src/components/CurrentlyPlaying.tsx
@@ -5,78 +5,84 @@ import { trpc } from '@lib/trpc';
 import Image from 'next/image';
 
 export default function CurrentlyPlaying() {
-	// @ts-expect-error
-	const currentlyPlaying = trpc.currentlyPlaying.useQuery({}, { refetchInterval: 1000 });
+    // @ts-expect-error
+    const currentlyPlaying = trpc.currentlyPlaying.useQuery({}, { refetchInterval: 1000 });
 
-	if (currentlyPlaying.data && currentlyPlaying.data.is_playing && currentlyPlaying.data.currently_playing_type === 'track') {
-		return (
-			<div className='fixed bottom-0 z-10 flex h-20 w-full items-center justify-between bg-card px-4 dark:bg-darkMode-card sm:grid sm:h-24 sm:grid-cols-3'>
-				<div className='flex gap-4'>
-					<div className='relative h-12 w-12 sm:h-16 sm:w-16'>
-						<Image
-							className='aspect-square max-w-none rounded-sm object-contain'
-							src={
-								// @ts-expect-error
-								currentlyPlaying.data?.item?.album.images[0].url
-							}
-							alt='Album Cover'
-							fill
-							sizes='3rem 4rem'
-						/>
-					</div>
-					<div className='flex flex-col justify-center'>
-						<a className='hover:underline' href={currentlyPlaying.data?.item?.external_urls.spotify} target='_blank' rel='noreferrer'>
-							{currentlyPlaying.data?.item?.name}
-						</a>
-						<div className='flex gap-1'>
-							{
-								// @ts-expect-error
-								currentlyPlaying.data?.item.artists.map((artist, index) => (
-									<div key={artist.id + index}>
-										<a className='text-xs hover:underline dark:text-gray-300' href={artist.external_urls.spotify} target='_blank' rel='noreferrer'>
-											{artist.name}
-										</a>
-										{
-											// @ts-expect-error
-											index < currentlyPlaying.data?.item.artists.length - 1 && ','
-										}
-									</div>
-								))
-							}
-						</div>
-					</div>
-				</div>
-				<div className='hidden justify-center gap-3 sm:grid'>
-					<div className='flex justify-center'>
-						<button disabled>
-							<FontAwesomeIcon height={40} width={40} icon={faBackwardStep} />
-						</button>
-						<button disabled>
-							<FontAwesomeIcon height={40} width={40} icon={faPause} />
-						</button>
-						<button disabled>
-							<FontAwesomeIcon height={40} width={40} icon={faForwardStep} />
-						</button>
-					</div>
-					<div className='flex items-center gap-4'>
-						<span className='text-xs'>{msToString(currentlyPlaying.data.progress_ms ?? 0)}</span>
-						<div className='h-1.5 w-52 rounded-full bg-white dark:bg-gray-600 md:w-96 lg:w-[30rem]'>
-							<div
-								className='h-1.5 rounded-full bg-black dark:bg-white'
-								style={{ width: `${((currentlyPlaying.data.progress_ms ?? 0) / (currentlyPlaying.data?.item?.duration_ms ?? 0)) * 100}%` }}
-							/>
-						</div>
-						<span className='text-xs'>{msToString(currentlyPlaying.data?.item?.duration_ms ?? 0)}</span>
-					</div>
-				</div>
-				<div className='hidden justify-end sm:flex'>
-					<button disabled>
-						<FontAwesomeIcon height={40} width={40} icon={faBars} />
-					</button>
-				</div>
-			</div>
-		);
-	}
+    if (currentlyPlaying.data && currentlyPlaying.data.is_playing && currentlyPlaying.data.currently_playing_type === 'track') {
+        return (
+            <div className='fixed bottom-0 z-10 flex h-20 w-full items-center justify-between bg-card px-4 dark:bg-darkMode-card sm:grid sm:h-24 sm:grid-cols-3'>
+                <div className='flex gap-4'>
+                    <div className='relative h-12 w-12 sm:h-16 sm:w-16'>
+                        <Image
+                            className='aspect-square max-w-none rounded-sm object-contain'
+                            src={
+                                // @ts-expect-error
+                                currentlyPlaying.data?.item?.album.images[0].url
+                            }
+                            alt='Album Cover'
+                            fill
+                            sizes='3rem 4rem'
+                        />
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <a className='hover:underline' href={currentlyPlaying.data?.item?.external_urls.spotify} target='_blank' rel='noreferrer'>
+                            {currentlyPlaying.data?.item?.name}
+                        </a>
+                        <div className='flex gap-1'>
+                            {
+                                // @ts-expect-error
+                                currentlyPlaying.data?.item.artists.map((artist, index) => (
+                                    <div key={artist.id + index}>
+                                        <a className='text-xs hover:underline dark:text-gray-300' href={artist.external_urls.spotify} target='_blank' rel='noreferrer'>
+                                            {artist.name}
+                                        </a>
+                                        {
+                                            // @ts-expect-error
+                                            index < currentlyPlaying.data?.item.artists.length - 1 && ','
+                                        }
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className='hidden justify-center gap-3 sm:grid'>
+                    <div className='flex justify-center'>
+                        <button disabled>
+                            <FontAwesomeIcon height={40} width={40} icon={faBackwardStep} />
+                        </button>
+                        <button disabled>
+                            <FontAwesomeIcon height={40} width={40} icon={faPause} />
+                        </button>
+                        <button disabled>
+                            <FontAwesomeIcon height={40} width={40} icon={faForwardStep} />
+                        </button>
+                    </div>
+                    <div className='flex items-center gap-4'>
+                        <span className='text-xs'>{msToString(currentlyPlaying.data.progress_ms ?? 0)}</span>
+                        <div className='h-1.5 w-52 rounded-full bg-white dark:bg-gray-600 md:w-96 lg:w-[30rem]'>
+                            <div
+                                className='h-1.5 rounded-full bg-black dark:bg-white'
+                                style={{ width: `${((currentlyPlaying.data.progress_ms ?? 0) / (currentlyPlaying.data?.item?.duration_ms ?? 0)) * 100}%` }}
+                            />
+                        </div>
+                        <span className='text-xs'>{msToString(currentlyPlaying.data?.item?.duration_ms ?? 0)}</span>
+                    </div>
+                </div>
+                <div className='hidden justify-end sm:flex'>
+                    <button disabled>
+                        <FontAwesomeIcon height={40} width={40} icon={faBars} />
+                    </button>
+                </div>
+                <div className='absolute bottom-0 h-1 w-11/12 rounded-full bg-white dark:bg-gray-600 sm:hidden'>
+                    <div
+                        className='h-1 rounded-full bg-black dark:bg-white'
+                        style={{ width: `${((currentlyPlaying.data.progress_ms ?? 0) / (currentlyPlaying.data?.item?.duration_ms ?? 0)) * 100}%` }}
+                    />
+                </div>
+            </div>
+        );
+    }
 
-	return <></>;
+    return <></>;
 }
