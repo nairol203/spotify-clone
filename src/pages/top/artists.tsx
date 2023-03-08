08281badd1b44189b0f25da65891b2b47fa82d61
Florian Bock <f.bock.2003@gmail.com>
2023-03-08 12:02:29 +0100

    .

--- src/pages/top/artists.tsx
+++ src/pages/top/artists.tsx
@@ -10,43 +10,6 @@ export default function Home() {
 	const [range, setRange] = useState<z.infer<typeof SPOTIFY_RANGE>>('short_term');
 	const topTracks = trpc.topArtists.useQuery({ range });
 
-	if (!topTracks.data)
-        return (
-            <div className='grid gap-4 py-4'>
-                <div className='flex'>
-                    <h1>Top Tracks</h1>
-                </div>
-                <div className='flex justify-center gap-10 sm:justify-start sm:gap-6'>
-                    <button
-                        className={`${range === 'short_term' ? 'underline' : 'opacity-80'} decoration-1 underline-offset-8 hover:underline`}
-                        onClick={() => setRange('short_term')}
-                    >
-                        4 Weeks
-                    </button>
-                    <button
-                        className={`${range === 'medium_term' ? 'underline' : 'opacity-80'} decoration-1 underline-offset-8 hover:underline`}
-                        onClick={() => setRange('medium_term')}
-                    >
-                        6 Months
-                    </button>
-                    <button
-                        className={`${range === 'long_term' ? 'underline' : 'opacity-80'} decoration-1 underline-offset-8 hover:underline`}
-                        onClick={() => setRange('long_term')}
-                    >
-                        All time
-                    </button>
-                </div>
-                <div>
-                    <div className='grid grid-cols-[1.25rem_1fr] items-center gap-4 px-4 py-2 text-gray-400'>
-                        <span className='flex justify-center'>#</span>
-                        <span>Titel</span>
-                    </div>
-                    <div className='mb-4 h-0.5 w-full rounded-full bg-gray-400 bg-opacity-10' />
-                    <SkeletonObjectDynamic count={25} type='album' ranking />
-                </div>
-            </div>
-        );
-
 	return (
 		<div className='grid gap-4 py-4'>
 			<h1>Top Artists</h1>
@@ -71,7 +34,7 @@ export default function Home() {
 				</div>
 				<div className='mb-4 h-0.5 w-full rounded-full bg-gray-400 bg-opacity-10' />
 				<div>
-					{topTracks.data.items.map((artist, index) => (
+					{topTracks.data ? topTracks.data.items.map((artist, index) => (
 						<div className='grid grid-cols-[1.25rem_1fr] items-center gap-4 rounded-[4px] px-4 py-2 md:hover:bg-white md:hover:bg-opacity-10' key={artist.id}>
 							<div className='flex w-5 justify-center'>{index + 1}</div>
 							<div className='flex items-center gap-4'>
@@ -81,7 +44,7 @@ export default function Home() {
 								</Link>
 							</div>
 						</div>
-					))}
+					)) : <SkeletonObjectDynamic count={25} type='album' ranking />}
 				</div>
 			</div>
 		</div>
--- src/pages/top/tracks.tsx
+++ src/pages/top/tracks.tsx
@@ -11,44 +11,6 @@ export default function Home() {
 	const [range, setRange] = useState<z.infer<typeof SPOTIFY_RANGE>>('short_term');
 	const topTracks = trpc.topTracks.useQuery({ range });
 	
-	if (!topTracks.data)
-        return (
-            <div className='grid gap-4 py-4'>
-                <div className='flex'>
-                    <h1>Top Tracks</h1>
-                </div>
-                <div className='flex justify-center gap-10 sm:justify-start sm:gap-6'>
-                    <button
-                        className={`${range === 'short_term' ? 'underline' : 'opacity-80'} decoration-1 underline-offset-8 hover:underline`}
-                        onClick={() => setRange('short_term')}
-                    >
-                        4 Weeks
-                    </button>
-                    <button
-                        className={`${range === 'medium_term' ? 'underline' : 'opacity-80'} decoration-1 underline-offset-8 hover:underline`}
-                        onClick={() => setRange('medium_term')}
-                    >
-                        6 Months
-                    </button>
-                    <button
-                        className={`${range === 'long_term' ? 'underline' : 'opacity-80'} decoration-1 underline-offset-8 hover:underline`}
-                        onClick={() => setRange('long_term')}
-                    >
-                        All time
-                    </button>
-                </div>
-                <div>
-                    <div className='grid grid-cols-[1.25rem_6fr_1fr] items-center gap-4 px-4 py-2 text-gray-400'>
-                        <span className='flex justify-center'>#</span>
-                        <span>Titel</span>
-                        <span className='flex justify-end'>Dauer</span>
-                    </div>
-                    <div className='mb-4 h-0.5 w-full rounded-full bg-gray-400 bg-opacity-10' />
-                    <SkeletonObjectDynamic count={25} type='track' ranking />
-                </div>
-            </div>
-        );
-
 	return (
 		<div className='grid gap-4 py-4'>
 			<h1>Top Tracks</h1>
@@ -74,7 +36,7 @@ export default function Home() {
 				</div>
 				<div className='mb-4 h-0.5 w-full rounded-full bg-gray-400 bg-opacity-10' />
 				<div>
-					{(topTracks.data.items.map((track, index) => (
+					{(topTracks.data ? topTracks.data.items.map((track, index) => (
 						<div className='flex  items-center justify-between gap-2 rounded-[4px] px-4 py-2 md:hover:bg-white md:hover:bg-opacity-10' key={track.id + index}>
 							<div className='flex items-center gap-4' key={track.id}>
 								<span className='flex w-5 justify-center'>{index + 1}</span>
@@ -98,7 +60,7 @@ export default function Home() {
 							</div>
 							<span className='sm:mr-2'>{msToString(track.duration_ms)}</span>
 						</div>
-					)))}
+					)) : <SkeletonObjectDynamic count={25} type='track' ranking />)}
 				</div>
 			</div>
 		</div>
