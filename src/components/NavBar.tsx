6aa74c15a6c43281eb610312718e8e42dbf398e0
Florian Bock <f.bock.2003@gmail.com>
2023-03-08 15:31:36 +0100

    user image when logged in

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,21 @@
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function NavBar() {
 	const router = useRouter();
+	const { data: session } = useSession();
 
 	return (
 		<nav className='flex h-full w-full items-center gap-8 border-b-[1px] border-b-[#282828]  bg-darkMode-card px-4 shadow-sm'>
-			<Link href='/'>
+			<Link href={session?.user.image ? `/user/${session.user.id}` :'/'}>
 				<Image
-					src='/logo.png'
+					src={session?.user.image ?? '/logo.png'}
 					alt='Logo von Nairol Price Check'
 					width={30}
 					height={30}
-					className='md:hover:underline md:hover:underline-offset-4 md:hover:brightness-110'
+					className='md:hover:underline md:hover:underline-offset-4 md:hover:brightness-110 rounded-full'
 				/>
 			</Link>
 			<Link className={`${router.pathname === '/' ? 'underline' : 'hover:opacity-70'} decoration-1 underline-offset-8 hover:underline`} href='/'>
